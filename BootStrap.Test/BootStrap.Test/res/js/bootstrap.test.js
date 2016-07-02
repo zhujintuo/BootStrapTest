2a6c35d69f33ca2e478a223db9cc007c125323ff
zhujintuo <942408056@qq.coom>
2016-07-02 21:16:58 +0800

    Modal end

--- BootStrap.Test/BootStrap.Test/res/js/bootstrap.test.js
+++ BootStrap.Test/BootStrap.Test/res/js/bootstrap.test.js
@@ -942,11 +942,149 @@ $(document).on('click.bs.collapse.data-api', '[data-toggle="collapse"]', functio
 
     Modal.prototype.backdrop=function(callback){
         var that=this;
+        var animate=this.$element.hasClass('fade')?'fade':'';
+
+        if(this.isShown&&this.options.backdrop){
+            var doAnimate=$.support.transition&&animate;
+
+            this.$backdrop=$(document.createElement('div'))
+                .addClass('modal-backdrop '+animate)
+                .appendTo(this.$body);
+
+            this.$element.on('click.dismiss.bs.modal',$.proxy(function(e){
+                if(this.ignoreBackdropClick){
+                    this.ignoreBackdropClick=false;
+                    return;
+                }
+
+                if(e.target!==e.currentTarget) return;
+                this.options.backdrop=='static'?this.$element[0].focus():this.hide();
+            },this));
+
+            if(doAnimate) this.$backdrop[0].offsetWidth;
+            this.$backdrop.addClass('in');
+            if(!callback) return;
+            doAnimate?this.$backdrop.one('bsTransitionEnd',callback)
+                .emulateTransitionEnd(Modal.BACKDROP_TRANSITION_DURATION):callback();
+        }else if(!this.isShown&&this.$backdrop){
+            this.$backdrop.removeClass('in');
+
+            var callbackRemove=function(){
+                that.removeBackdrop();
+                callback&&callback();
+            }
+
+            $.support.transition&&this.$element.hasClass('fade')?
+                this.$backdrop
+                    .one('bsTransitionEnd',callbackRemove)
+                    .emulateTransitionEnd(Modal.BACKDROP_TRANSITION_DURATION):callbackRemove();
+        }else if(callback){
+            callback();
+        }
     }
+
+    Modal.prototype.handleUpdate=function(){
+        this.adjustDialog();
+    }
+
+    Modal.prototype.adjustDialog=function(){
+        var modalIsOverflowing=this.$element[0].scrollHeight>document.documentElement.clientHeight;
+
+        this.$element.css({
+            paddingLeft:  !this.bodyIsOverflowing && modalIsOverflowing ? this.scrollbarWidth : '',
+            paddingRight: this.bodyIsOverflowing && !modalIsOverflowing ? this.scrollbarWidth : ''
+        });
+    }
+
+    Modal.prototype.resetAdjustments=function(){
+        this.$element.css({
+            paddingLeft:'',
+            paddingRight:''
+        });
+    }
+
+    Modal.prototype.checkScrollbar=function(){
+        var fullWindowWidth=window.innerWidth;
+        if(!fullWindowWidth){
+            var documentElementRect=document.documentElement.getBoundingClientRect();
+            fullWindowWidth=documentElementRect.right-Math.abs(documentElementRect.left);
+        }
+
+        this.bodyIsOverflowing=document.body.clientHeight<fullWindowWidth;
+        this.scrollbarWidth=this.measureScrollbar();
+    }
+
+    Modal.prototype.setScrollbar=function(){
+        var bodyPad=parseInt((this.$body.css('padding-right')||0),10);
+        this.originalBodyPad=document.body.style.paddingRight||'';
+        if(this.bodyIsOverflowing) this.$body.css('padding-right',bodyPad+this.scrollbarWidth);
+    }
+
+    Modal.prototype.resetScrollbar=function(){
+        this.$body.css('padding-right',this.originalBodyPad);
+    }
+
+    Modal.prototype.measureScrollbar=function(){
+        var scrollDiv=document.createElement('div');
+        scrollDiv.className='modal-scrollbar-measure';
+        this.$body.append(scrollDiv);
+
+        var scrollbarWidth=scrollDiv.offsetWidth-scrollDiv.clientWidth;
+        this.$body[0].removeChild(scrollDiv);
+        return scrollbarWidth;
+    }
+
+    function Plugin(option,_relatedTarget){
+        return this.each(function(){
+            var $this=$(this);
+            var data=$this.data('bs.modal');
+            var options =$.extend({},Modal.DEFAULTS,$this.data(),typeof option=='object'&&option);
+
+            if(!data) $this.data('bs.modal',(data=new Modal(this,options)));
+            if(typeof option=='string') data[option](_relatedTarget);
+            else if(options.show) data.show(_relatedTarget);
+        });
+    }
+
+    var old=$.fn.modal;
+    $.fn.modal=Plugin;
+    $.fn.modal.Constructor=Modal;
+
+    $.fn.modal.noConflict=function(){
+        $.fn.modal=old;
+        return this;
+    }
+
+    $(document).on('click.bs.modal.data-api','[data-toggle="modal"]',function(e){
+        var $this=$(this);
+        var href=$this.attr('href');
+        var $targer=$($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))); // strip for ie7
+        var option  = $target.data('bs.modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data());
+
+        if($this.is('a')) e.preventDefault();
+        $targer.one('show.bs.modal',function(showEvent){
+            if(showEvent.isDefaultPrevented()) return;
+            $targer.one('hidden.bs.modal',function(){
+                $this.is(':visible')&&$this.trigger('focus');
+            });
+        });
+
+        Plugin.call($targer,option,this);
+    });
 }(jQuery);
  
  
- 
+/* ========================================================================
+ * Bootstrap: tooltip.js v3.3.5
+ * http://getbootstrap.com/javascript/#tooltip
+ * Inspired by the original jQuery.tipsy by Jason Frame
+ * ========================================================================
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+
+
  
  
  
